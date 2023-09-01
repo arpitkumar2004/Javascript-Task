0e89c7138fefebcb7d97ba5b5a9998117c10d429
Arpit-Kumar <kumararpit17773@gmail.com>
2023-09-01 12:20:27 +0530

    All Bugs soved now

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let secretNumber = Math.trunc(20 * Math.random() + 1);
 let highscore = 0;
 let time = [0, 0];
 let timerInterval;
+let gameWon = false;
 
 document.querySelector('.again').addEventListener('click', function () {
     time = [0, 0];
@@ -11,17 +12,24 @@ document.querySelector('.again').addEventListener('click', function () {
     document.getElementById('timer').innerHTML = '';
     document.querySelector('.check').style.backgroundColor = '#f1356d';
 
+    // Reset the background color to white
+    document.querySelector("body").style.backgroundColor = 'white';
+
     // Clear the timer interval when starting a new game
     clearInterval(timerInterval);
+    timerInterval = null; // Reset the timerInterval
 
     // Clear the message
     document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector('.number').style.width = '15rem'; // Reset the number width
+    document.querySelector('.number').textContent = '?'; // Reset the number
+
+    gameWon = false; // Reset the game won status
 });
 
 document.querySelector('.check').addEventListener('click', () => {
     let guess = Number(document.querySelector('.guess').value);
 
-    // Start the timer when the "Check!" button is clicked
     if (!timerInterval) {
         timerInterval = setInterval(() => {
             time[1]++;
@@ -43,6 +51,8 @@ document.querySelector('.check').addEventListener('click', () => {
             highscore = Number(document.querySelector('.score').textContent);
         }
         document.querySelector('.highscore').textContent = highscore;
+        gameWon = true; // Mark the game as won
+        clearInterval(timerInterval); // Stop the clock
     } else if (guess > secretNumber) {
         document.querySelector(".message").textContent = "Too high";
         document.querySelector('.score').textContent--;
@@ -51,7 +61,7 @@ document.querySelector('.check').addEventListener('click', () => {
         document.querySelector('.score').textContent--;
     }
 
-    if (Number(document.querySelector('.score').textContent) <= 0) {
+    if (Number(document.querySelector('.score').textContent) <= 0 && !gameWon) {
         document.querySelector(".message").textContent = "You lost the game";
         clearInterval(timerInterval);
         document.getElementById('timer').innerHTML = '';
